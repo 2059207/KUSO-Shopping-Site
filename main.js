cb47c8ed869fb3ac4f3977fa20c98d1ebb8f0b92
2059207 <77169618+2059207@users.noreply.github.com>
2022-07-21 05:15:57 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+let array = ["kan"];
+console.log(array);
+
+function mail(kann){
+    let message = '';
+
+    message = '送信されました。';
+    console.log(message);
+
+    const p_mail = document.querySelector('#result');
+    p_mail.textContent = message;
+}
